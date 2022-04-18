676d682105ba4424beced91a956ac753334bd5a8
Daniel <danielpietruszynski@wp.pl>
2022-04-18 18:15:33 +0200

    checking

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const width = 8
     const squares = []
+    let score = 0
 
     const candyColors = [
         'red',
@@ -79,8 +80,83 @@ document.addEventListener('DOMContentLoaded', () => {
         } else if (squareIdBeingReplaced && !validMove) {
             squares[squareIdBeingReplaced].style.backgroundColor = colorBeingReplaced
             squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
-        } else square[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced   
-        
+        } else square[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced
     }
+    
+    function checkRowForFour() {
+        for (i = 0; i < 60; i++) {
+            let rowOfFour = [i, i+1, i+2, i+3]
+            let decidedColor = squares[i].style.backgroundColor
+            const isBlank = squares[i].style.backgroundColor === ''
+
+            const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55]
+            if (notValid.includes(i)) continue
+            
+            if (rowOfFour.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 4
+                rowOfFour.forEach(index => {
+                    squares[index].style.backgroundColor = ''
+                })
+            }
+        } 
+    }
+    checkRowForFour()
+
+    function checkColumnForFour() {
+        for (i = 0; i < 47; i++) {
+            let columnOfFour = [i, i+width, i+width*2, i+width*3]
+            let decidedColor = squares[i].style.backgroundColor
+            const isBlank = squares[i].style.backgroundColor === ''
+
+            if (columnOfFour.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 4
+                columnOfFour.forEach(index => {
+                    squares[index].style.backgroundColor = ''
+                })
+            }
+        } 
+    }
+    checkColumnForFour()
+
+    function checkRowForThree() {
+        for (i = 0; i < 61; i++) {
+            let rowOfThree = [i, i+1, i+2]
+            let decidedColor = squares[i].style.backgroundColor
+            const isBlank = squares[i].style.backgroundColor === ''
+
+            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
+            if (notValid.includes(i)) continue
+            
+            if (rowOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 3
+                rowOfThree.forEach(index => {
+                    squares[index].style.backgroundColor = ''
+                })
+            }
+        } 
+    }
+    checkRowForThree()
+
+    function checkColumnForThree() {
+        for (i = 0; i < 47; i++) {
+            let columnOfThree = [i, i+width, i+width*2]
+            let decidedColor = squares[i].style.backgroundColor
+            const isBlank = squares[i].style.backgroundColor === ''
+
+            if (columnOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 3
+                columnOfThree.forEach(index => {
+                    squares[index].style.backgroundColor = ''
+                })
+            }
+        } 
+    }
+
+    window.setInterval(function() {
+        checkRowForFour()
+        checkColumnForFour()
+        checkRowForThree()
+        checkColumnForThree()
+    }, 100)
 
 })

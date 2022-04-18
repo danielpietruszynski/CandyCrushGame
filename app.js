2d5d1ab9624ce28423b9bc0001845d05a03ce58d
Daniel <danielpietruszynski@wp.pl>
2022-04-18 17:22:49 +0200

    random colors

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 8
     const squares = []
 
+    const candyColors = [
+        'red',
+        'purple',
+        'blue',
+        'yellow',
+        'orange',
+        'green'
+    ]
+
     function createBoard() {
         for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
+            let randomColor = Math.floor(Math.random() * candyColors.length)
+            square.style.backgroundColor = candyColors [randomColor]
             grid.appendChild(square)
             squares.push(square)
         }
